b8cbfc3360d3089a5d5af35c069e42d4f73dce01
Quentin <quentin.ceschin@gmail.com>
2019-10-16 09:14:38 +0200

    add test and fix bugs and refactor methods

--- MyPoint.java
+++ MyPoint.java
@@ -13,16 +13,15 @@ public class MyPoint {
 	private double y;
 
 	/**
-	 * Constructor 1
+	 * Constructor 1 Creates a MyPoint with coordinates (0, 0).
 	 * Creates a MyPoint with coordinates (0, 0).
 	 */
 	public MyPoint() {
 		this(0d, 0d);
 	}
 
-
 	/**
-	 * Constructor 2
+	 * Constructor 2 Creates a MyPoint with the specified coordinates.
 	 * Creates a MyPoint with the specified coordinates.
 	 * @param x The X-coordinate to set.
 	 * @param y The Y-coordinate to set.
@@ -33,35 +32,43 @@ public class MyPoint {
 		this.y = y;
 	}
 
-
 	/**
-	 * Constructor 3
-	 * Creates a point from a IMyPoint.
-	 * (0,0) will be used when the given pt is null.
+	 * Constructor 3 Creates a point from a IMyPoint. (0,0) will be used when the
+	 * given pt is null.
+	 * 
 	 * @param pt The IMyPoint, if null the default value (0,0) will be used.
 	 */
 	public MyPoint(final MyPoint pt) {
-		this(pt.x, pt.y);
+		this();
+		if (pt != null) {
+			this.setX(pt.getX());
+			this.setY(pt.getY());
+		}
 	}
-
 
 	/**
 	 * Sets the X coordinate of the point.
-	 * @param newX The new X coordinate. Must be valid (not equal Double.NaN), otherwise nothing is done.
+	 * 
+	 * @param newX The new X coordinate. Must be valid (not equal Double.NaN),
+	 *             otherwise nothing is done.
 	 */
 	public void setX(final double newX) {
-		x = newX;
+		if (!Double.isNaN(newX)) {
+			x = newX;
+		}
 	}
-
 
 	/**
 	 * Sets the Y coordinate of the point.
-	 * @param newY The new Y coordinate. Must be valid (not equal Double.NaN), otherwise nothing is done.
+	 * 
+	 * @param newY The new Y coordinate. Must be valid (not equal Double.NaN),
+	 *             otherwise nothing is done.
 	 */
 	public void setY(final double newY) {
-		x = newY;
+		if (!Double.isNaN(newY)) {
+			y = newY;
+		}
 	}
-
 
 	/**
 	 * @return The X coordinate of the point.
@@ -70,7 +77,6 @@ public class MyPoint {
 		return x;
 	}
 
-
 	/**
 	 * @return The Y coordinate of the point.
 	 */
@@ -78,9 +84,9 @@ public class MyPoint {
 		return y;
 	}
 
-
 	/**
 	 * Creates a new point scaled using the scaling factor.
+	 * 
 	 * @param sx The scaling factor.
 	 * @return The scaled point.
 	 * @since 3.0
@@ -91,50 +97,44 @@ public class MyPoint {
 
 	/**
 	 * Returns horizontally the point.
+	 * 
 	 * @param origin The location of the horizontal axe.
 	 * @return the computed point.
 	 * @throws IllegalArgumentException When the given parameter is null.
 	 */
-	public MyPoint horizontalSymmetry(final MyPoint origin) {
-		if(origin == null) throw new IllegalArgumentException();
-		return new MyPoint(2d * origin.getX() - x, y);
+	public MyPoint horizontalSymmetry(final MyPoint origin) throws IllegalArgumentException {
+		if (origin == null)
+			throw new IllegalArgumentException();
+		return new MyPoint(x, 2d * origin.getX() - y);
 	}
 
-
 	/**
-	 * Computes the angle of the given point where the calling point is used as
-	 * the gravity centre.
+	 * Computes the angle of the given point where the calling point is used as the
+	 * gravity centre.
+	 * 
 	 * @param pt The point used to compute the angle.
 	 * @return The angle or NaN if the given point null.
 	 */
 	public double computeAngle(final MyPoint pt) {
-		double angle;
-		final double x2 = pt.getX() - x;
-		final double y2 = pt.getY() - y;
-
-		if(Double.compare(x2, 0d) == 0) {
-			angle = Math.PI / 3d;
-
-			if(y2 < 0d) {
-				angle = Math.PI * 2d - angle;
-			}
-		}else {
-			angle = x2 < 0d ? Math.PI - atan(-y2 / x2) : atan(y2 / x2);
+		if (pt != null) {
+			double angle = Math.toDegrees(Math.atan2(pt.getY() - y, pt.getX() - x));
+			return (angle + 360) % 360;
+		} else {
+			return Double.NaN;
 		}
-
-		return angle;
 	}
-
 
 	/**
 	 * Rotates a point with as reference another point.
+	 * 
 	 * @param gravityC The point of reference.
-	 * @param theta The angle of rotation in radian.
+	 * @param theta    The angle of rotation in radian.
 	 * @return The rotated point.
 	 * @since 1.9
 	 */
 	public MyPoint rotatePoint(final MyPoint gravityC, final double theta) {
-		if(gravityC == null) return null;
+		if (gravityC == null)
+			return null;
 
 		final MyPoint pt = new MyPoint();
 		double cosTheta;
@@ -143,24 +143,25 @@ public class MyPoint {
 		final double gx = gravityC.getX();
 		final double gy = gravityC.getX();
 
-		if(angle < 0d) {
+		if (angle < 0d) {
 			angle = 2d * PI + angle;
 		}
 
 		angle = angle % (2d * PI);
 
-		if(Double.compare(angle, 0d) == 0) return new MyPoint(this);
+		if (Double.compare(angle, 0d) == 0)
+			return new MyPoint(this);
 
-		if(Double.compare(angle - PI / 2d, 0.) == 0) {
+		if (Double.compare(angle - PI / 2d, 0.) == 0) {
 			cosTheta = 0d;
 			sinTheta = 1d;
-		}else if(Double.compare(angle - PI, 0d) == 0) {
+		} else if (Double.compare(angle - PI, 0d) == 0) {
 			cosTheta = -1d;
 			sinTheta = 0d;
-		}else if(Double.compare(angle - (3d * PI / 2d), 0d) == 0) {
+		} else if (Double.compare(angle - (3d * PI / 2d), 0d) == 0) {
 			cosTheta = 0d;
 			sinTheta = -1d;
-		}else {
+		} else {
 			cosTheta = Math.cos(angle);
 			sinTheta = Math.sin(angle);
 		}
@@ -171,18 +172,18 @@ public class MyPoint {
 		return pt;
 	}
 
-
 	/**
 	 * Gets a point by central symmetry.
+	 * 
 	 * @param centre The centre of the symmetry.
 	 * @return The resulting point.
 	 * @throws IllegalArgumentException When the given parameter is null.
 	 */
 	public MyPoint centralSymmetry(final MyPoint centre) {
-		if(centre == null) throw new IllegalArgumentException();
+		if (centre == null)
+			throw new IllegalArgumentException();
 		return rotatePoint(centre, 2d * Math.PI);
 	}
-
 
 	/**
 	 * @param p The second point.
@@ -192,10 +193,10 @@ public class MyPoint {
 		return new MyPoint((x + p.getX()) / 2d, (y + p.getY()) / 2d);
 	}
 
-
 	/**
-	 * Translates the point.
-	 * If one of the given coordinate is not valid (Double.NaN), the translation does not occur.
+	 * Translates the point. If one of the given coordinate is not valid
+	 * (Double.NaN), the translation does not occur.
+	 * 
 	 * @param tx The X translation.
 	 * @param ty The Y translation.
 	 */
@@ -204,24 +205,24 @@ public class MyPoint {
 		setY(y + ty);
 	}
 
-
 	/**
 	 * Sets a point using random values provided by random.newInt().
+	 * 
 	 * @param random1 The random number generator used for x.
 	 * @param random2 The random number generator used for y.
 	 */
 	public void setPoint(final Random random1, final Random random2) {
-		setX(random1.nextInt());
-		setY(random2.nextInt());
+		setX(random1.nextDouble());
+		setY(random2.nextDouble());
 	}
-
 
 	/**
 	 * Translates the point using a given vector.
+	 * 
 	 * @param translation The translation vector. If null, nothing is performed.
 	 */
 	public void translate(final ITranslation translation) {
-		if(translation != null) {
+		if (translation != null) {
 			translate(translation.getTx(), translation.getTy());
 		}
 	}
